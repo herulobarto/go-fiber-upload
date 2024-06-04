3b0a5a75f2afda33fdc4fd8c7b63f32f42485e52
herulobarto <herulobarto@gmail.com>
2024-06-04 15:42:46 +0700

    membuat logic identifikasi file dan membuat logic upload file ke folder baru dengan nama yang sudah diubah

--- go.mod
+++ go.mod
@@ -9,10 +9,14 @@ require (
 	github.com/gofiber/template/html/v2 v2.1.1 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/gravityblast/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	github.com/howeyc/fsnotify v0.9.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
+	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
@@ -13,6 +23,82 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", nil)
+	})
+
+	app.Post("/upload", func(c *fiber.Ctx) error {
+		var input struct {
+			Nama_gambar string
+		}
+
+		if err := c.BodyParser(&input); err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+
+		gambar, err := c.FormFile("gambar")
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+
+		// mengambil nama file
+		fmt.Printf("Nama file: %s \n", gambar.Filename)
+
+		// mengambil ukuran file
+		fmt.Printf("Ukuran file(bytes): %d \n", gambar.Size)
+
+		fmt.Printf("Ukuran file(kilobytes): %d \n", gambar.Size/1024)
+
+		fmt.Printf("Ukuran file(megabytes): %f \n", (float64(gambar.Size)/1024)/1024)
+
+		// mengambil mime type
+		fmt.Printf("Mime type: %s \n", gambar.Header.Get("Content-Type"))
+
+		// mengambil ekstensi file
+		splitDots := strings.Split(gambar.Filename, ".")
+		ext := splitDots[len(splitDots)-1]
+		fmt.Println(ext)
+
+		// mengganti nama file nya dengan tanggal
+		namaFileBaru := fmt.Sprintf("%s.%s", time.Now().Format("2006-01-02-15-04-05"), ext)
+		fmt.Println(namaFileBaru)
+
+		// mengambil ukuran gambar
+		fileHeader, _ := gambar.Open()
+		defer fileHeader.Close()
+
+		imageConfig, _, err := image.DecodeConfig(fileHeader)
+		if err != nil {
+			log.Print(err)
+		}
+
+		width := imageConfig.Width
+		height := imageConfig.Height
+		fmt.Printf("width: %d \n", width)
+		fmt.Printf("height: %d \n", height)
+
+		// membuat folder upload
+		folderUpload := filepath.Join(".", "uploads")
+
+		// mkdirAll => proses membuat folder
+		if err := os.MkdirAll(folderUpload, 0770); err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		// menyimpan gambar ke direktori uploads
+		if err := c.SaveFile(gambar, "./uploads/"+namaFileBaru); err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		return c.JSON(fiber.Map{
+			"title":       input.Nama_gambar,
+			"nama_gambar": namaFileBaru,
+			"message":     "Gambar berhasil diupload",
+		})
 	})
 
 	app.Listen(":8080")
